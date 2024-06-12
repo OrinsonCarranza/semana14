01231340f25342a85298c2e5377553d9ccd3a19d
OrinsonCarranza <orinsoncarranza@gmail.com>
2024-06-11 22:52:59 -0500

    Empresa Semana 05

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -8,6 +8,7 @@
     <nav>
         <a href="/">Inicio</a> -
         <a href="{{ route('personas.index') }}">Personas</a> -
+        <a href="{{ route('personas.create') }}">Crear Persona</a> -
         <a href="{{ route('contacto') }}">Contacto</a>
     </nav>
     <p>Puedes ponerte en contacto con nosotros a través del siguiente formulario:</p>
--- resources/views/personas/index.blade.php
+++ resources/views/personas/index.blade.php
@@ -21,6 +21,7 @@
     <nav>
         <a href="/">Inicio</a> -
         <a href="{{ route('personas.index') }}">Personas</a> -
+        <a href="{{ route('personas.create') }}">Crear Persona</a> -
         <a href="{{ route('contacto') }}">Contacto</a>
     </nav>
     <table>
