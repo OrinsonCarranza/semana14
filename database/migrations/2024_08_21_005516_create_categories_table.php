cece14947a3491eaf6df9e22166d1ceda60a0a9a
OrinsonCarranza <orinsoncarranza@gmail.com>
2024-08-20 23:36:17 -0500

    Semana 14

---
+++ database/migrations/2024_08_21_005516_create_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table -> string('name');
+            $table -> string('url')->unique();
+            $table->timestamps();
+        });
+        Schema::table('persona', function(Blueprint $table){
+            $table->unsignedBiInteger('category_id')->nullable()->after('id');
+
+            //
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
